21aa89741691d53fcb66135d68586b25a5ed72c7
Mark Meschi <mark.meschi@gmail.com>
2017-07-09 04:35:12 -0500

    initial commit

---
+++ mdmApp1/server.R
@@ -0,0 +1,27 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+function(input, output) {
+   
+  data <- reactive({rnorm(input$num)})
+  
+  output$hist <- renderPlot( {
+    hist(data())
+  })
+  
+  output$txt <- renderPrint( {
+    summary(data())
+  })
+  
+}
+
+
---
+++ mdmApp1/ui.R
@@ -0,0 +1,18 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+fluidPage(
+  
+  sliderInput("num", "Choose a Number", 1, 100, 50), 
+  plotOutput("hist"),
+  verbatimTextOutput("txt")
+)
